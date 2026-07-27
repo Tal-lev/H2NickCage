3536ff549001ce33d49930ea7c3ae17185225c65
Tal-lev <ch6tal@gmail.com>
2026-07-27 19:54:44 +0300

    first commit

---
+++ src/ready.lua
@@ -0,0 +1,18 @@
+---@meta _
+-- globals we define are private to our plugin!
+---@diagnostic disable: lowercase-global
+
+-- here is where your mod sets up all the things it will do.
+-- this file will not be reloaded if it changes during gameplay
+-- 	so you will most likely want to have it reference
+--	values and functions later defined in `reload.lua`.
+
+--Loading the package at every room
+modutil.mod.Path.Wrap("SetupMap", function(base, source, args)
+	LoadPackages({ Name = "JarlUlsfark-Nicky" })
+	return base(source, args)
+end)
+
+modutil.once_loaded.game(function()
+
+end)
